5b0add587bc329adcfa44d8aad7e5c82ed1ad935
ramrayankula <ramchowdary96@gmail.com>
2022-11-26 20:57:18 +0530

    daily log update

--- log.sh
+++ log.sh
@@ -14,6 +14,13 @@ then
 	sudo cp /var/log/httpd/error_log  /home/vagrant/logs/errorlog/"error_log_$(date +"%F %T")"
 	
 	echo "Log files copied successfully !!"	
+
+	cd /home/vagrant/logs/
+	git add .
+	git commit -m "daily log update"
+	git push origin master
+	echo "files uploaded to git successfulyy !!"
+
 else
 	echo "Script exited without copying anything"
 fi
