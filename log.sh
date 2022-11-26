4d0df811b7d25a5f7f3d04f1d40b1e6d3704ad06
ramrayankula <ramchowdary96@gmail.com>
2022-11-26 20:47:53 +0530

    first commit

---
+++ log.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+read -p "Do you want to take back up of httpd log files(y/n): " s
+if [ $s == y ]
+then
+	
+	echo "Copying Httpd Server Access_log and Error_log files in backup folder) "
+	
+	cd /home/vagrant/logs/accesslog/
+	sudo touch "access_log_$(date +"%F %T")"
+	sudo cp /var/log/httpd/access_log  /home/vagrant/logs/accesslog/"access_log_$(date +"%F %T")"
+
+	cd /home/vagrant/logs/errorlog/
+	sudo touch "error_log_$(date +"%F %T")"
+	sudo cp /var/log/httpd/error_log  /home/vagrant/logs/errorlog/"error_log_$(date +"%F %T")"
+	
+	echo "Log files copied successfully !!"	
+else
+	echo "Script exited without copying anything"
+fi
